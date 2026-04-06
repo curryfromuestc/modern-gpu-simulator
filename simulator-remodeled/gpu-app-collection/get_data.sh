f3d00c93c4e8f17d613cc4c4250799c609ba3380
curryfromuestc <curryfromuestc@gmail.com>
2026-04-06 09:45:14 +0800

    fix:Download data

--- simulator-remodeled/gpu-app-collection/get_data.sh
+++ simulator-remodeled/gpu-app-collection/get_data.sh
@@ -4,50 +4,78 @@ export BASH_ROOT="$( cd "$( dirname "$BASH_SOURCE" )" && pwd )"
 DATA_SUBDIR="/data_dirs/"
 DATA_ROOT=$BASH_ROOT$DATA_SUBDIR
 
-if [ ! -d $DATA_ROOT ]; then
-    wget https://github.com/upc-arco/gpu-app-collection-simulation-dataset/releases/download/v1.0/other-apps-part1.tar.gz
-    wget https://github.com/upc-arco/gpu-app-collection-simulation-dataset/releases/download/v1.0/other-apps-part2.tar.gz
-    wget https://github.com/upc-arco/gpu-app-collection-simulation-dataset/releases/download/v1.0/other-apps-part3.tar.gz
-    wget https://github.com/upc-arco/gpu-app-collection-simulation-dataset/releases/download/v1.0/other-apps-part4.tar.gz
-    wget https://github.com/upc-arco/gpu-app-collection-simulation-dataset/releases/download/v1.0/other-apps-part5.tar.gz
-    wget https://github.com/upc-arco/gpu-app-collection-simulation-dataset/releases/download/v1.0/other-apps-part6.tar.gz
-    wget https://github.com/upc-arco/gpu-app-collection-simulation-dataset/releases/download/v1.0/other-apps-part7.tar.gz
-    wget https://github.com/upc-arco/gpu-app-collection-simulation-dataset/releases/download/v1.0/other-apps-part8.tar.gz
-    wget https://github.com/upc-arco/gpu-app-collection-simulation-dataset/releases/download/v1.0/other-apps-part9.tar.gz
-    wget https://github.com/upc-arco/gpu-app-collection-simulation-dataset/releases/download/v1.0/other-apps-part10.tar.gz
-    wget https://github.com/upc-arco/gpu-app-collection-simulation-dataset/releases/download/v1.0/tango-data-part1.tar.gz
-    wget https://github.com/upc-arco/gpu-app-collection-simulation-dataset/releases/download/v1.0/tango-data-part2.tar.gz
-    mkdir -p $DATA_ROOT
-    tar xzvf other-apps-part1.tar.gz
-    tar xzvf other-apps-part2.tar.gz
-    tar xzvf other-apps-part3.tar.gz
-    tar xzvf other-apps-part4.tar.gz
-    tar xzvf other-apps-part5.tar.gz
-    tar xzvf other-apps-part6.tar.gz
-    tar xzvf other-apps-part7.tar.gz
-    tar xzvf other-apps-part8.tar.gz
-    tar xzvf other-apps-part9.tar.gz
-    tar xzvf other-apps-part10.tar.gz
-    tar xzvf tango-data-part1.tar.gz -C $DATA_ROOT
-    tar xzvf tango-data-part2.tar.gz -C $DATA_ROOT
-    rm other-apps-part1.tar.gz
-    rm other-apps-part2.tar.gz
-    rm other-apps-part3.tar.gz
-    rm other-apps-part4.tar.gz
-    rm other-apps-part5.tar.gz
-    rm other-apps-part6.tar.gz
-    rm other-apps-part7.tar.gz
-    rm other-apps-part8.tar.gz
-    rm other-apps-part9.tar.gz
-    rm other-apps-part10.tar.gz
-    rm tango-data-part1.tar.gz
-    rm tango-data-part2.tar.gz
-    mv data_dirs/tango/CifarNet data_dirs/tango/Tango-CN
-    mv data_dirs/tango/ResNet data_dirs/tango/Tango-RN
-    mv data_dirs/tango/LSTM data_dirs/tango/Tango-LSTM
-    mv data_dirs/tango/GRU data_dirs/tango/Tango-GRU
-    mv data_dirs/tango/AlexNet data_dirs/tango/Tango-AN
-    mv data_dirs/tango/SqueezeNet data_dirs/tango/Tango-SN
-    mkdir -p $DATA_ROOT/cuda/lonestargpu-2.0/lonestar-bfs-wla
-    ln -s $DATA_ROOT/cuda/lonestargpu-2.0/inputs $DATA_ROOT/cuda/lonestargpu-2.0/lonestar-bfs-wla/data
+usage() {
+    echo "Usage: $0 -d <dataset>"
+    echo ""
+    echo "Required:"
+    echo "  -d <dataset>   Dataset to download"
+    echo ""
+    echo "Available datasets:"
+    echo "  other-apps     GPU application benchmarks (10 parts)"
+    echo "  tango          Tango DNN benchmarks (2 parts)"
+    echo ""
+    echo "Examples:"
+    echo "  $0 -d other-apps"
+    echo "  $0 -d tango"
+    exit 1
+}
+
+DATASET=""
+while getopts "d:h" opt; do
+    case $opt in
+        d) DATASET="$OPTARG" ;;
+        h) usage ;;
+        *) usage ;;
+    esac
+done
+
+if [ -z "$DATASET" ]; then
+    echo "Error: You must specify a dataset with -d"
+    echo ""
+    usage
 fi
+
+BASE_URL="https://github.com/upc-arco/gpu-app-collection-simulation-dataset/releases/download/v1.0"
+
+download_other_apps() {
+    mkdir -p "$DATA_ROOT"
+    for i in $(seq 1 10); do
+        TARFILE="other-apps-part${i}.tar.gz"
+        wget "${BASE_URL}/${TARFILE}"
+        tar xzvf "$TARFILE"
+        rm "$TARFILE"
+    done
+    mkdir -p "$DATA_ROOT/cuda/lonestargpu-2.0/lonestar-bfs-wla"
+    ln -s "$DATA_ROOT/cuda/lonestargpu-2.0/inputs" \
+          "$DATA_ROOT/cuda/lonestargpu-2.0/lonestar-bfs-wla/data"
+}
+
+download_tango() {
+    mkdir -p "$DATA_ROOT"
+    for i in 1 2; do
+        TARFILE="tango-data-part${i}.tar.gz"
+        wget "${BASE_URL}/${TARFILE}"
+        tar xzvf "$TARFILE" -C "$DATA_ROOT"
+        rm "$TARFILE"
+    done
+    mv "$DATA_ROOT/tango/CifarNet"   "$DATA_ROOT/tango/Tango-CN"
+    mv "$DATA_ROOT/tango/ResNet"     "$DATA_ROOT/tango/Tango-RN"
+    mv "$DATA_ROOT/tango/LSTM"       "$DATA_ROOT/tango/Tango-LSTM"
+    mv "$DATA_ROOT/tango/GRU"        "$DATA_ROOT/tango/Tango-GRU"
+    mv "$DATA_ROOT/tango/AlexNet"    "$DATA_ROOT/tango/Tango-AN"
+    mv "$DATA_ROOT/tango/SqueezeNet" "$DATA_ROOT/tango/Tango-SN"
+}
+
+case "$DATASET" in
+    other-apps)
+        download_other_apps
+        ;;
+    tango)
+        download_tango
+        ;;
+    *)
+        echo "Error: Unknown dataset '$DATASET'"
+        echo "Available datasets: other-apps, tango"
+        exit 1
+        ;;
+esac
--- simulator-remodeled/util/hw_stats/get_hw_data.sh
+++ simulator-remodeled/util/hw_stats/get_hw_data.sh
@@ -1,80 +1,95 @@
+#!/bin/bash
 export DATA_ROOT="$( cd "$( dirname "$BASH_SOURCE" )" && pwd )"
 export DATA_ROOT=$DATA_ROOT/../../hw_run
 
-mkdir -p $DATA_ROOT
+usage() {
+    echo "Usage: $0 -c <card>"
+    echo ""
+    echo "Required:"
+    echo "  -c <card>   GPU card to download hw data for"
+    echo ""
+    echo "Available cards:"
+    echo "  ampere-rtx3070"
+    echo "  turing-rtx2060"
+    echo "  pascal-titanx"
+    echo "  pascal-p100"
+    echo "  pascal-1080ti"
+    echo "  fermi-gtx480"
+    echo "  volta-titanv"
+    echo "  volta-quadro-v100"
+    echo "  volta-tesla-v100"
+    echo "  kepler-titan"
+    echo ""
+    echo "Examples:"
+    echo "  $0 -c ampere-rtx3070"
+    echo "  $0 -c volta-titanv"
+    exit 1
+}
 
-TITANX_PASCAL_DATA=$DATA_ROOT/TITAN-X-PASCAL
-P100_PASCAL_DATA=$DATA_ROOT/TESLA-P100
-FERMI_GTX480_DATA=$DATA_ROOT/GTX480
-TI1080_DATA=$DATA_ROOT/1080TI_PASCAL
-TITANV_DATA=$DATA_ROOT/TITANV
-QUADRO_V100_DATA=$DATA_ROOT/QUADRO-V100
-TESLA_V100_DATA=$DATA_ROOT/TESLA-V100
-TURING_RTX_DATA=$DATA_ROOT/TURING-RTX2060
-KEPLER_DATA=$DATA_ROOT/KEPLER-TITAN
-AMPERE_RTX_DATA=$DATA_ROOT/AMPERE-RTX3070
+CARD=""
+while getopts "c:h" opt; do
+    case $opt in
+        c) CARD="$OPTARG" ;;
+        h) usage ;;
+        *) usage ;;
+    esac
+done
 
-if [ ! -d $AMPERE_RTX_DATA ]; then
-    TARFILE=ampere.rtx3070.tgz
-    wget https://engineering.purdue.edu/tgrogers/gpgpu-sim/hw_data/$TARFILE
-    tar -xzvf $TARFILE -C $DATA_ROOT
-    rm $TARFILE
+if [ -z "$CARD" ]; then
+    echo "Error: You must specify a GPU card with -c"
+    echo ""
+    usage
 fi
 
-if [ ! -d $TURING_RTX_DATA ]; then
-    TARFILE=turing.rtx2060.cycle.tgz
-    wget https://engineering.purdue.edu/tgrogers/gpgpu-sim/hw_data/$TARFILE
-    tar -xzvf $TARFILE -C $DATA_ROOT
-    rm $TARFILE
+get_tarfile() {
+    case "$1" in
+        ampere-rtx3070)    echo "ampere.rtx3070.tgz" ;;
+        turing-rtx2060)    echo "turing.rtx2060.cycle.tgz" ;;
+        pascal-titanx)     echo "pascal.titanx.cycle.tgz" ;;
+        pascal-p100)       echo "pascal.tesla.p100.cycles.tgz" ;;
+        pascal-1080ti)     echo "pascal.1080ti.cycle.tgz" ;;
+        fermi-gtx480)      echo "fermi.gtx480.cycle.tgz" ;;
+        volta-titanv)      echo "volta.titanv.tgz" ;;
+        volta-quadro-v100) echo "quadro.v100.cycle.tgz" ;;
+        volta-tesla-v100)  echo "tesla.v100.tgz" ;;
+        kepler-titan)      echo "kepler.titan.cycle.tgz" ;;
+    esac
+}
+
+get_datadir() {
+    case "$1" in
+        ampere-rtx3070)    echo "AMPERE-RTX3070" ;;
+        turing-rtx2060)    echo "TURING-RTX2060" ;;
+        pascal-titanx)     echo "TITAN-X-PASCAL" ;;
+        pascal-p100)       echo "TESLA-P100" ;;
+        pascal-1080ti)     echo "1080TI_PASCAL" ;;
+        fermi-gtx480)      echo "GTX480" ;;
+        volta-titanv)      echo "TITANV" ;;
+        volta-quadro-v100) echo "QUADRO-V100" ;;
+        volta-tesla-v100)  echo "TESLA-V100" ;;
+        kepler-titan)      echo "KEPLER-TITAN" ;;
+    esac
+}
+
+TARFILE=$(get_tarfile "$CARD")
+DATADIR=$(get_datadir "$CARD")
+
+if [ -z "$TARFILE" ]; then
+    echo "Error: Unknown card '$CARD'"
+    echo ""
+    usage
 fi
 
-if [ ! -d $TITANX_PASCAL_DATA ]; then
-    wget https://engineering.purdue.edu/tgrogers/gpgpu-sim/hw_data/pascal.titanx.cycle.tgz
-    tar -xzvf pascal.titanx.cycle.tgz -C $DATA_ROOT
-    rm pascal.titanx.cycle.tgz
+mkdir -p "$DATA_ROOT"
+
+FULL_DATA_PATH="$DATA_ROOT/$DATADIR"
+if [ -d "$FULL_DATA_PATH" ]; then
+    echo "Data already exists: $FULL_DATA_PATH"
+    echo "Skipping download."
+    exit 0
 fi
 
-if [ ! -d $P100_PASCAL_DATA ]; then
-    wget https://engineering.purdue.edu/tgrogers/gpgpu-sim/hw_data/pascal.tesla.p100.cycles.tgz
-    tar -xzvf pascal.tesla.p100.cycles.tgz -C $DATA_ROOT
-    rm pascal.tesla.p100.cycles.tgz
-fi
-
-if [ ! -d $FERMI_GTX480_DATA ]; then
-    wget https://engineering.purdue.edu/tgrogers/gpgpu-sim/hw_data/fermi.gtx480.cycle.tgz
-    tar -xzvf fermi.gtx480.cycle.tgz -C $DATA_ROOT
-    rm fermi.gtx480.cycle.tgz
-fi
-
-if [ ! -d $TI1080_DATA ]; then
-    wget https://engineering.purdue.edu/tgrogers/gpgpu-sim/hw_data/pascal.1080ti.cycle.tgz
-    tar -xzvf pascal.1080ti.cycle.tgz -C $DATA_ROOT
-    rm pascal.1080ti.cycle.tgz
-fi
-
-if [ ! -d $TITANV_DATA ]; then
-    wget https://engineering.purdue.edu/tgrogers/gpgpu-sim/hw_data/volta.titanv.tgz
-    tar -xzvf volta.titanv.tgz -C $DATA_ROOT
-    rm volta.titanv.tgz
-fi
-
-if [ ! -d $QUADRO_V100_DATA ]; then
-    TARFILE=quadro.v100.cycle.tgz
-    wget https://engineering.purdue.edu/tgrogers/gpgpu-sim/hw_data/$TARFILE
-    tar -xzvf $TARFILE -C $DATA_ROOT
-    rm $TARFILE
-fi
-
-if [ ! -d $TESLA_V100_DATA ]; then
-    TARFILE=tesla.v100.tgz
-    wget https://engineering.purdue.edu/tgrogers/gpgpu-sim/hw_data/$TARFILE
-    tar -xzvf $TARFILE -C $DATA_ROOT
-    rm $TARFILE
-fi
-
-if [ ! -d $KEPLER_DATA ]; then
-    TARFILE=kepler.titan.cycle.tgz
-    wget https://engineering.purdue.edu/tgrogers/gpgpu-sim/hw_data/$TARFILE
-    tar -xzvf $TARFILE -C $DATA_ROOT
-    rm $TARFILE
-fi
+BASE_URL="https://engineering.purdue.edu/tgrogers/gpgpu-sim/hw_data"
+wget "$BASE_URL/$TARFILE"
+tar -xzvf "$TARFILE" -C "$DATA_ROOT"
+rm "$TARFILE"
